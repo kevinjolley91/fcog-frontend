cc23b151dcac70926d4fd60a598cf880d7b1d12d
kevinjolley91 <jolley427@gmail.com>
2024-09-05 13:46:14 -0400

    updates membersshow for updating member info

--- src/MembersShow.jsx
+++ src/MembersShow.jsx
@@ -6,6 +6,17 @@ export function MembersShow(props) {
       <p>Name: {props.member.name}</p>
       <p>Phone Number: {props.member.phone_number}</p>
       <p>Position: {props.member.position}</p>
+      <form>
+        <div>
+          Name: <input defaultValue={props.member.name} name="name" type="text" />
+        </div>
+        <div>
+          Phone Number: <input defaultValue={props.member.phone_number} name="phone_number" type="text" />
+        </div>
+        <div>
+          Position: <input defaultValue={props.member.position} name="position" type="text" />
+        </div>
+      </form>
     </div>
   )
 }
